b50066b45803edb74f455e6bb22c63425fff563a
Simon Traels Ravn <cs2@cs2.dk>
2013-11-20 19:38:00 +0000

    Basket should allow for 500 units

--- app/overrides/_line_item_deface.rb
+++ app/overrides/_line_item_deface.rb
@@ -1,5 +1,5 @@
 Deface::Override.new(:virtual_path  => "spree/orders/_line_item",
                      :surround => "erb:contains('line_item_quantity')",
-                     :text          => '<% if variant.product.set_count and variant.product.set_count > 1 %><%= item_form.select :quantity, options_for_select((0..200).reject{|i| i % item_form.object.variant.product.set_count != 0 }, item_form.object.quantity), :class => "line_item_quantity" %><% else %><%= render_original %><% end %>',
+                     :text          => '<% if variant.product.set_count and variant.product.set_count > 1 %><%= item_form.select :quantity, options_for_select((0..500).reject{|i| i % item_form.object.variant.product.set_count != 0 }, item_form.object.quantity), :class => "line_item_quantity" %><% else %><%= render_original %><% end %>',
                      :name          => "cart_select_as_quantity"
                      )
